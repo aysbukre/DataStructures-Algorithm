bd57139ffd0c15a88b51a130dbb0ae4f14ea3d1b
aysbukre <aysbukre@gmail.com>
2023-10-29 15:38:50 +0300

    4.5

--- DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedList.cs
+++ DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedList.cs
@@ -9,6 +9,7 @@ namespace DataStructures.LinkedList.SinglyLinkedList
     public class SinglyLinkedList<T>
     {
         public SinglyLinkedListNode<T> Head { get; set; }
+        private bool isHeadNull => Head == null;
         // liste başına ekleme işlemi
         // algoritma yeni node'un pointerını mevcut head'i işaret edecek şekilde güncellenir
         //yani yeni node head olarak işaretlenir. bir tane yeni düğüm tasarımı oluşturulacak.
@@ -19,8 +20,10 @@ namespace DataStructures.LinkedList.SinglyLinkedList
             newNode.Next = Head;
             Head = newNode;
         }
+        
         //liste sonuna ekleme işlemi
 
+        /*
         public  void AddLast (T value) 
         { 
             var newNode = new SinglyLinkedList<T>(value);
@@ -32,11 +35,46 @@ namespace DataStructures.LinkedList.SinglyLinkedList
             }
 
             var current = Head;
-            while (current != null)
+            while (current.Next!= null)
             {
                 current= current.Next;
             }
             current.Next = newNode;
         }
+        */
+        //listenin arasına ekleme
+        public void AddAfter(SinglyLinkedListNode<T> node, T value)
+        {
+            //node ifadesi bir düğümdür ve boş olamaz eğer boş olursa diye bir hata gönderiyoruz.
+            if (node == null)
+            {
+                throw new ArgumentNullException();
+                
+            }
+            // bu listesinin boş olmaması lazim ki başka bir düğüm eklensin bunun için de uyarı gönderiyoruz.
+            if (isHeadNull)
+            {
+                AddFirst(value);
+                return;
+            }
+            //SinglyLinkedListNode<T> node burada bir referans düğümü vermem gerekiyor ismini de node yaptık
+            // T sadece bir değer 
+            //önce bir düğüm oluşturacağım
+            var newNode = new SinglyLinkedListNode<T>((T)value);    
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Equals(node))
+                {
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                    return;
+                }
+                current = current.Next;
+            }
+            throw new ArgumentException("The reference node not exist");
+
+        }
+
     }
 }
